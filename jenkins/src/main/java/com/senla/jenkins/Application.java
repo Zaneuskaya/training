27b25c938702539fa4c7326d4a3b88d40340083f
Zaneuskaya <aliaksandra.zaneuskaya@gmail.com>
2019-08-26 14:45:44 +0300

    main class

---
+++ jenkins/src/main/java/com/senla/jenkins/Application.java
@@ -0,0 +1,13 @@
+package com.senla.jenkins;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+
+
+}
