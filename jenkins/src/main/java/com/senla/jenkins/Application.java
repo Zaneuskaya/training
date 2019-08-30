54d4ee72ac27e96f3ba52962bcf5c13b70bfe2a3
Zaneuskaya <aliaksandra.zaneuskaya@gmail.com>
2019-08-30 16:04:27 +0300

    build test

--- jenkins/src/main/java/com/senla/jenkins/Application.java
+++ jenkins/src/main/java/com/senla/jenkins/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("!!!!!!!!!!!!!!!!!Hello from new version");
         SpringApplication.run(Application.class, args);
     }
 }
